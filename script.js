e854bcb2acf73aab89a77137eefa9ae11773d34c
Princekumar <pk181299@gmail.com>
2024-01-02 12:58:15 +0530

    about

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ menuBtn.addEventListener('click', () => {
 
     navbar.classList.toggle("active");
 })
+
+
+let touch = document.querySelector(".touch");
+
+let links = document.querySelector(".links");
+
+touch.addEventListener('click', () => {
+
+    links.classList.toggle("active-links");
+})
