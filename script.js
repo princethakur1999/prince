94a8380b51cbfb68b41f0f4639c6c1bb7e1a8666
Princekumar <pk181299@gmail.com>
2024-01-05 17:32:57 +0530

    theme change

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
 let menuBtn = document.querySelector(".menu-btn");
+
 let navbar = document.querySelector(".navbar");
 
 
 menuBtn.addEventListener('click', () => {
 
     navbar.classList.toggle("active");
-})
+    menuBtn.classList.toggle("color");
 
-
-let touch = document.querySelector(".touch");
-
-let links = document.querySelector(".links");
-
-touch.addEventListener('click', () => {
-
-    links.classList.toggle("active-links");
-})
+});
