b857b22f040bc04341775114ef543b9ec0da8bc5
princethakur1999 <pk181299@gmail.com>
2024-08-29 16:57:37 +0530

    new theme added

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let menuBtn = document.querySelector(".menu-btn");
 let navbar = document.querySelector(".navbar");
 
-
 menuBtn.addEventListener('click', () => {
 
     navbar.classList.toggle("active");
@@ -19,3 +18,22 @@ touch.addEventListener('click', () => {
 })
 
 
+let navItems = document.getElementsByClassName("nav-item");
+
+for (let i = 0; i < navItems.length; i++) {
+
+    navItems[i].addEventListener('click', () => {
+
+        navbar.classList.toggle("active");
+
+    });
+}
+
+
+document.addEventListener('scroll', () => {
+    
+    links.classList.remove("open-close");
+    navbar.classList.remove("active");
+
+
+})
